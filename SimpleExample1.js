7eb102b12744948a4b42a72785d05c4c705f3ee9
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-07-20 07:14:39 -0400

    Create SimpleExample1.js

---
+++ SimpleExample1.js
@@ -0,0 +1,13 @@
+// Here is a simple code example to try playing with in Expo and on Local Premises
+
+
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ padding: 50 }}>
+      <Text>ready...</Text>
+    </View>
+  );
+}
